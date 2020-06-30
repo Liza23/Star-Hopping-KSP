e9936d21904837225e8012dd8d3aa30b04481440
Liza Dahiya <53112806+Liza23@users.noreply.github.com>
2020-06-30 11:11:16 +0530

    Update Hop_sequence (modular).py

--- Week 4/changes/Hop_sequence (modular).py
+++ Week 4/changes/Hop_sequence (modular).py
@@ -1,62 +1,61 @@
 import math
 import pandas as pd
 
-# we create a class Point() that have attributes x any y and a function to find distance between those points.
-
 class Point():
   def __init__(self, x, y):
     self.x = x
     self.y = y
 
   def distance(self,b):
-    x_term = int(self.x) - int(b.x)
-    y_term = int(self.y) - int(b.y)
+    x_term = float(self.x) - float(b.x)
+    y_term = float(self.y) - float(b.y)
     return math.sqrt((x_term * x_term) + (y_term * y_term))
-    
-
-# we craete a class hop_func() that have attributes as limit_range, max_stars, limiting_brightness, click
-# hence for every click this class have functions that return a hop which can be stored in a hop_sequence 
-# to generate a star plot.
-
-# this value is used to avoid returning of hops even when there is no nearby star (e.g. editor clicking
-# somewhere near the borders).
-
-# max_stars is the limit put to remove unnessecory faint stars from our star list
-
-# limiting_brightness is the minimum brightness below which we consider a star to be faint and is used to 
-# remove stars unnecessary faint stars if the no of stars go up by max_stars
-
-class hop_func():
+  
+  
+  class hop_func():
   
   def __init__(self, limit_range, max_stars, limiting_brightness, x, y):
+    # this value is used to avoid returning of hops even 
+    # when there is no nearby star (e.g. editor clicking
+    # somewhere near the borders).
     self.limit_range = limit_range
+    
+    # max_stars is the limit put to remove unnessecory 
+    # faint stars from our star list
     self.max_stars = max_stars
+    
+    # limiting_brightness is the minimum brightness below 
+    # which we consider a star to be faint
     self.limiting_brightness = limiting_brightness
+    
     self.click = Point(x, y)
+    
 
   # the function list_star() returns a list of stars that lie 
   # within the sqaure formed with the click as center
-  
   def list_stars(self, data):
     stars_catalogue = pd.DataFrame(columns = ['star','ra','dec','brightness'])
 
-    l_x_limit = int(self.click.x) - self.limit_range
-    r_x_limit = int(self.click.x) + self.limit_range
-    l_y_limit = int(self.click.y) - self.limit_range
-    r_y_limit = int(self.click.y) + self.limit_range
+    l_x_limit = float(self.click.x) - self.limit_range
+    r_x_limit = float(self.click.x) + self.limit_range
+    l_y_limit = float(self.click.y) - self.limit_range
+    r_y_limit = float(self.click.y) + self.limit_range
+
+    data = data.sort_values(by = ['ra', 'dec'])
   
     for i in range(len(data)):    
-      if (data['ra'][i] > l_x_limit and data['ra'][i] < r_x_limit) and (data['dec'][i] > l_y_limit and data['dec'][i] < r_y_limit):
+      while (data['ra'][i] > l_x_limit and data['ra'][i] < r_x_limit) and (data['dec'][i] > l_y_limit and data['dec'][i] < r_y_limit):
         stars_catalogue = stars_catalogue.append({'star': data['star'][i], 'ra': data['ra'][i], 'dec': data['dec'][i], 'brightness': data['brightness'][i]}, ignore_index=True)
+        break
 
     if len(stars_catalogue) >= self.max_stars:
        stars_catalogue = stars_catalogue[stars_catalogue['brightness'] > self.limiting_brightness]
 
     return stars_catalogue
 
-  # the function min_distance() will return the value of the nearest 
-  # star to the clicks from the selected stars.  
   
+  # the function min_distance() will return the value of the nearest 
+  # star to the clicks from the selected stars. 
   def min_distance(self, star_catalogue):
     star_name = None
 
@@ -76,14 +75,18 @@ class hop_func():
       count = 0
       # to keep a track of stars which are at a same value from our click
   
-      for i in range(len(stars_point)):
+      for i in range(0, len(stars_point)):
         if min_val >= self.click.distance(stars_point[i]):
           min_val = self.click.distance(stars_point[i])
           min_star = stars_point[i]
           star_name = star_catalogue['star'][i]
+
+      for i in range(len(stars_point)):
+        if min_val == self.click.distance(stars_point[i]):
           count += 1
-    
+
       assert(count == 1)
+      
 
       return min_star, star_name
   
@@ -104,30 +107,28 @@ class hop_func():
     star_point, star_name = self.min_distance(star_catalogue)
     hops = {'star': star_name, 'ra': star_point.x, 'dec': star_point.y}
     return hops
-    
+  
 # Driver code with temp data
-data = {'star': ['a', 'b', 'c', 'd'], 'ra': [100, 200, 300, 400], 'dec': [10, 20, 30, 40], 'brightness': [0.1, 0.2, 0.3, 0.4]}
+data = {'star': ['a', 'b', 'c', 'd'], 'ra': [100, 50, 400, 120], 'dec': [10, 20, 40, 20], 'brightness': [0.1, 0.2, 0.3, 0.4]}
 df = pd.DataFrame (data, columns = ['star','ra','dec','brightness'])
 
-def main():
+# uncomment these lines to load data and add original url as well
+# df = pd.read_csv('url')
+# df.head()
+
+def save_hops():
   hops = pd.DataFrame(columns = ['star','ra','dec'])
   flag = True
   while(flag):
     x = input("x = ")
     y = input("y = ")
     flag = input("flag = ")
-    
-    # these parameters can be changed as per data required.
-    # limit_range = 50, max_range = 50, limiting_brightness = 0.1
-    hop_stars = hop_func(50, 50, 0.1, x, y)
-    
-    # hop_star is class hop_func() that have functions that return hopped star for every click.
+    hop_stars = hop_func(50, 50 , 0.1, x, y)
+
     stars_catalogue = hop_stars.list_stars(df)
     hopped_star = hop_stars.hop_near_click(stars_catalogue, hops)
-    
-    # finally save all hops in a dataframe to return finally
     hops = hops.append(hopped_star, ignore_index = True)
 
   return hops
 
-print(main())
+print(save_hops())
